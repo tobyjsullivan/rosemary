73d1dd669b38438d62304537f7f22fdcb0e2592b
Toby Sullivan <toby.sullivan@hootsuite.com>
2014-03-20 22:58:46 -0700

    Added method to lookAt specific point

--- src/main/scala/scalatron/botwar/botPlugin/Vision.scala
+++ src/main/scala/scalatron/botwar/botPlugin/Vision.scala
@@ -1,10 +1,29 @@
 package scalatron.botwar.botPlugin
 
 class Vision(input: String) {
+  require(input.size > 0, "Input must not be an empty string")
   require(input.filterNot(c => "MmSsBbPpW_?".contains(c)).size == 0, "The input string includes unacceptable characters")
 
   val size: Int = math.sqrt(input.size).toInt
   require(size * size == input.size, "Length of input string must be a perfect square")
 
-  val center = Point(size/2, size/2)
+  lazy val center = Point(size/2, size/2)
+  lazy val range = size/2
+  
+  def lookAt(position: Point): Char = {
+    require(position.x >= 0-range)
+    require(position.x <= range)
+    require(position.y >= 0-range)
+    require(position.y <= range)
+
+    val idx = relPositionAsIndex(position)
+    input(idx)
+  }
+  
+  private def relPositionAsIndex(position: Point) = {
+    val absX = position.x + range
+    val absY = position.y + range
+
+    size * absY + absX
+  }
 }
--- src/test/scala/scalatron/botwar/botPlugin/VisionSpec.scala
+++ src/test/scala/scalatron/botwar/botPlugin/VisionSpec.scala
@@ -5,8 +5,8 @@ import org.scalatest.Matchers._
 
 class VisionSpec extends FlatSpec with Matchers {
   "constructor" should "throw an exception if length of input is not a perfect square" in {
-    val view = Set(
-      "__S_p",
+    val view = List(
+      "__s_p",
       "W___b",
       "P_MS_",
       "WWW_B",
@@ -19,8 +19,8 @@ class VisionSpec extends FlatSpec with Matchers {
   }
 
   it should "throw an exception if the view contains any invalid characters" in {
-    val view = Set(
-      "__S_p",
+    val view = List(
+      "__s_p",
       "W_L_b", // There is an L on this line
       "P_MS_",
       "WWW_B",
@@ -33,8 +33,8 @@ class VisionSpec extends FlatSpec with Matchers {
   }
 
   it should "not throw an exception if the input is valid" in {
-    val view = Set(
-      "__S_p",
+    val view = List(
+      "__s_p",
       "W___b",
       "P_MS_",
       "WWW_B",
@@ -44,9 +44,9 @@ class VisionSpec extends FlatSpec with Matchers {
     new Vision(view.mkString)
   }
 
-  "size" should "be equal to the square root of the input string" in {
-    val view = Set(
-      "__S_p",
+  "size" should "be equal to 5 for a 5x5 input" in {
+    val view = List(
+      "__s_p",
       "W___b",
       "P_MS_",
       "WWW_B",
@@ -59,8 +59,8 @@ class VisionSpec extends FlatSpec with Matchers {
   }
 
   "center" should "be 3,3 for an input of 5x5" in {
-    val view = Set(
-      "__S_p",
+    val view = List(
+      "__s_p",
       "W___b",
       "P_MS_",
       "WWW_B",
@@ -70,5 +70,63 @@ class VisionSpec extends FlatSpec with Matchers {
     val vision = new Vision(view.mkString)
 
     vision.center should equal (Point(2, 2))
+  }
+
+  "range" should "be 2 for an input of 5x5" in {
+    val view = List(
+      "__s_p",
+      "W___b",
+      "P_MS_",
+      "WWW_B",
+      "????_"
+    )
+
+    val vision = new Vision(view.mkString)
+
+    vision.range should equal (2)
+  }
+
+  "lookAt" should "return the correct value for a given position within view" in {
+    val view = List(
+      "__s_p",
+      "W___b",
+      "P_MS_",
+      "WWW_B",
+      "????_"
+    )
+
+    val vision = new Vision(view.mkString)
+
+    vision.lookAt(Point(2, -1)) should equal ('b')
+    vision.lookAt(Point(0, 0)) should equal ('M')
+    vision.lookAt(Point(-1, 1)) should equal ('W')
+    vision.lookAt(Point(0, -2)) should equal ('s')
+  }
+
+  it should "throw an exception for a position that is outside of view" in {
+    val view = List(
+      "__s_p",
+      "W___b",
+      "P_MS_",
+      "WWW_B",
+      "????_"
+    )
+
+    val vision = new Vision(view.mkString)
+
+    intercept[IllegalArgumentException]{
+      vision.lookAt(Point(-3, 0))
+    }
+    intercept[IllegalArgumentException]{
+      vision.lookAt(Point(0, -3))
+    }
+    intercept[IllegalArgumentException]{
+      vision.lookAt(Point(3, 0))
+    }
+    intercept[IllegalArgumentException]{
+      vision.lookAt(Point(0, 3))
+    }
+
+
   }
 }
