974dff32ac2cf8ffa20a94a71c4a9bc55bb78e29
Toby Sullivan <toby.sullivan@hootsuite.com>
2014-03-20 20:23:41 -0700

    Removed some template comments

--- src/main/scala/scalatron/botwar/botPlugin/BotController.scala
+++ src/main/scala/scalatron/botwar/botPlugin/BotController.scala
@@ -1,8 +1,5 @@
 package scalatron.botwar.botPlugin
 
-/**
- * Created by tobys on 2014-03-20.
- */
 object BotController {
   def respond(input: String): String = ""
 }
--- src/main/scala/scalatron/botwar/botPlugin/ControlFunctionFactory.scala
+++ src/main/scala/scalatron/botwar/botPlugin/ControlFunctionFactory.scala
@@ -1,9 +1,5 @@
 package scalatron.botwar.botPlugin
 
-/**
- * Created by tobys on 2014-03-20.
- */
-
 object ControlFunctionFactory {
   def create : (String => String) = BotController.respond
 }
