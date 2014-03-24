828295cd889873ec0f4adb91137fb40aead614a7
Toby Sullivan <tobyjsullivan@gmail.com>
2014-03-23 23:23:47 -0700

    Fixed some formatting

--- src/main/scala/BotController.scala
+++ src/main/scala/BotController.scala
@@ -16,34 +16,34 @@ object BotController {
 
   private def respondToReact(react: Command): String = {
     try {
-    val state = new State(react)
+      val state = new State(react)
 
-    // Look for food
-    val nearbyZugar = state.view.map(v => v.findAll('P')).getOrElse(Set())
+      // Look for food
+      val nearbyZugar = state.view.map(v => v.findAll('P')).getOrElse(Set())
 
-    // If zugar is near, move towards. Otherwise random
-    val dir = nearbyZugar.toList match {
-      case List() => {
-        val rand = new Random()
-        Point(rand.nextInt(3)-1, rand.nextInt(3)-1)
+      // If zugar is near, move towards. Otherwise random
+      val dir = nearbyZugar.toList match {
+        case List() => {
+          val rand = new Random()
+          Point(rand.nextInt(3) - 1, rand.nextInt(3) - 1)
+        }
+        case _ => Hippocampus.findClosest(Point(0, 0), nearbyZugar).truncate
       }
-      case _ => Hippocampus.findClosest(Point(0,0), nearbyZugar).truncate
-    }
 
-    // Gen a list of possible moves
-    val moves = for {
-      x:Int <- (-1 to 1).toSet
-      y:Int <- -1 to 1
-      point = Point(x, y)
-    } yield point
+      // Gen a list of possible moves
+      val moves = for {
+        x: Int <- (-1 to 1).toSet
+        y: Int <- -1 to 1
+        point = Point(x, y)
+      } yield point
 
-    // Find the best available move that is not blocked
-    val view = state.view.get
-    val bestDir = Hippocampus.findClosest(dir, moves.filter(p => view.lookAt(p) != 'W' && view.lookAt(p) != 'p'))
+      // Find the best available move that is not blocked
+      val view = state.view.get
+      val bestDir = Hippocampus.findClosest(dir, moves.filter(p => view.lookAt(p) != 'W' && view.lookAt(p) != 'p'))
 
-    val cmd = Command("Move", Map("direction" -> bestDir.toDirectionString))
+      val cmd = Command("Move", Map("direction" -> bestDir.toDirectionString))
 
-    cmd.toString
+      cmd.toString
     } catch {
       case e: Exception => {
         println(e.getMessage())
