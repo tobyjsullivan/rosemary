e43eec6d06512d6d951af1b21b41b38051537b15
Toby Sullivan <tobyjsullivan@gmail.com>
2014-03-24 00:48:22 -0700

    Only scared of really close Snorgs now

--- src/main/scala/Cerebrum.scala
+++ src/main/scala/Cerebrum.scala
@@ -3,7 +3,7 @@ import scala.util.Random
 class Cerebrum(state: State) {
   private val rand = new Random()
 
-  private lazy val nearbySnorgs = state.view.map(v => v.findAll('b')).getOrElse(Set())
+  private lazy val nearbySnorgs = state.view.map(v => v.findAll('b').filter(p => Hippocampus.distance(Point(0,0), p) <= 3.0)).getOrElse(Set())
   private lazy val nearbyEnemyMinibots = state.view.map(v => v.findAll('s')).getOrElse(Set())
   private lazy val nearbyZugar = state.view.map(v => v.findAll('P')).getOrElse(Set())
   private lazy val nearbyFluppets = state.view.map(v => v.findAll('B')).getOrElse(Set())
@@ -37,9 +37,16 @@ class Cerebrum(state: State) {
   }
 
   lazy val bestDirection: Point = (escapeRoute, foodDirection) match {
-    case (Some(d), _) => Hippocampus.findClosest(d, openHeadings)
-    case (None, Some(d)) => d
+    case (Some(d), _) => {
+      println("I'm Scared!")
+      Hippocampus.findClosest(d, openHeadings)
+    }
+    case (None, Some(d)) => {
+      println("I see food!")
+      d
+    }
     case (None, None) => {
+      println("I'm exploring")
       // If no threats and no food, choose an open heading that is free to explore
       openHeadings.toList(rand.nextInt(openHeadings.size))
     }
